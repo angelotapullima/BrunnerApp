36cf0f2d3f789ee6dc8357b7596eb0ff21815feb
Christian Zavaleta <christianzavaleta20@gmail.com>
2022-07-19 17:39:12 -0500

    .

--- lib/src/page/Mantenimiento/Lista de verificacion/Consulta Informacion/resultados_consulta.dart
+++ lib/src/page/Mantenimiento/Lista de verificacion/Consulta Informacion/resultados_consulta.dart
@@ -140,7 +140,7 @@ class ResultadosConsulta extends StatelessWidget {
             color: Colors.transparent.withOpacity(0.2),
             spreadRadius: 2,
             blurRadius: 5,
-            offset: const Offset(0, 3), // changes position of shadow
+            offset: const Offset(0, 3),
           ),
         ],
       ),
